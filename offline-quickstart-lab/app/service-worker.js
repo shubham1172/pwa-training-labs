e29fc479a8b5e8f0179de04af749b3564c6af314
Shubham Sharma <shubhamsharma1172@gmail.com>
2019-12-07 18:57:49 +0530

    offline-quickstart-labs

--- offline-quickstart-lab/app/service-worker.js
+++ offline-quickstart-lab/app/service-worker.js
@@ -1,1 +1,38 @@
+const cacheName = 'cache-v1'; // any arbitrary name
+const precacheResources = [
+  '/',
+  'index.html',
+  'styles/main.css',
+  'images/space1.jpg',
+  'images/space2.jpg',
+  'images/space3.jpg'
+];
 
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    // open a cache object and add precache stuff in it
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    // return cachedResponse if cache is valid, fetch otherwise
+    caches.match(event.request)
+    .then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+    );
+});
